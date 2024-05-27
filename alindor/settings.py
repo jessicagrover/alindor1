dfe1823eb511ccf4165d0f65bf3de902886ac852
Jessica Grover <102952877+jessicagrover@users.noreply.github.com>
2024-05-27 10:49:16 -0700

    Update settings.py
    new

--- alindor/settings.py
+++ alindor/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-
+SECRET_KEY = "django-insecure-a!k4amrxle-rq%evvv9fyn$!41su6r)tp2xs#(j8fmyv80lmuq"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
